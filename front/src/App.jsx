aa834d77e3b85039ba0fd026bc5afa833e95ba2c
Ivan Andrade <ivandandrade90@gmail.com>
2021-08-30 16:04:13 -0300

    Router package installed, and Router created in App.jsx. Login component directory created.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,26 @@
 import Home from './components/home/index';
+import Login from './components/login/index';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ } from 'react-router-dom';
 import './App.css';
 
 function App(){
   return(
-    <div className="mainContainer">
-      <div className="homeMainContainer">
-        <Home className="home" />
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <div className="homeMainContainer">
+            <Home />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
